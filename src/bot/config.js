d1340ac718a312cd10cc28884709e20207002a81
brown <86018533+2brownc@users.noreply.github.com>
2023-09-21 14:39:02 +0530

    chat bot init

---
+++ src/bot/config.js
@@ -0,0 +1,7 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  initialMessages: [createChatBotMessage(`Hello, welcome to student info system!`)],
+};
+
+export default config;
